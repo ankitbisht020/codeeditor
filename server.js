be9244bdc94c69548ea96990e108c75ab611d5c2
ankitbisht <ankitbisht9837@gmail.com>
2024-10-13 20:31:09 +0530

    kya karu

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
